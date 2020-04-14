27b08698befeb993fa70dd3c4ffd70941de51ac0
UO257192 <35766169+UO257192@users.noreply.github.com>
2020-04-14 19:54:39 +0200

    SDI-NodeJS-10.2- SW API REST Autenticación

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 let express = require('express');
 let app = express();
 
+var jwt = require('jsonwebtoken');
+app.set('jwt', jwt);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
+
 let fs = require('fs');
 let https = require('https');
 
@@ -25,6 +38,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 let gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app, mongo);
+
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    var token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
 
 // routerUsuarioSession
 let routerUsuarioSession = express.Router();
@@ -118,17 +165,17 @@ app.get('/', function (req, res) {
 });
 
 app.use(function (err, req, res, next) {
-   console.log("Error producido: "+ err);
-   if(!res.headersSent){
-       res.status(400);
-       res.send("Recurso no disponible");
-   }
+    console.log("Error producido: " + err);
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
 });
 
 //lanzar el servidor
 https.createServer({
     key: fs.readFileSync('certificates/alice.key'),
     cert: fs.readFileSync('certificates/alice.crt')
-}, app).listen(app.get('port'), function() {
+}, app).listen(app.get('port'), function () {
     console.log("Servidor activo");
 });
